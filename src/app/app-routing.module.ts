f041e9205fbb1280d16444ff51af114e35813184
Leandro Lopes <leandro.lopes@confitec.com.br>
2018-10-26 11:43:52 -0300

    removido referencia cobertura/cobertura-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from '@angular/router';
 import { CoberturaCorretoraComponent } from './cobertura-corretora/cobertura-corretora.component';
 import { HomeComponent} from "./home/home.component";
-import { CoberturaListComponent} from "./coberturas/cobertura-list/cobertura-list.component"
 //#endregion
 
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    { path: 'cobertura', component: CoberturaCorretoraComponent },
-    { path: 'list', component: CoberturaListComponent }
+    { path: 'cobertura', component: CoberturaCorretoraComponent }
 ];
 
 @NgModule({
